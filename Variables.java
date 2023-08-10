5baaddb21908765c1900bde9ec96e1d4785c9edf
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-08-11 00:08:44 +0530

    Hello world and variables

---
+++ Variables.java
@@ -0,0 +1,9 @@
+class Variables {
+    public static void main(String args[]) {
+        int num1 = 1;
+        int num2 = 2;
+        int answer = num1 + num2;
+
+        System.out.println(answer);
+    }
+}
