c3edd3d88203e2fd8d264a97f2a9598b91943f15
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-08-13 01:04:50 +0530

    primitive data types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+class PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        int num = 12312; // 4bytes
+        byte by = 8; // 1byte
+        short sh = 123; // 2bytes
+        long l = 12312333l; // 8 bytes
+
+        double d = 123.23; // 8 bytes
+        float f = 123.23f; // 4 bytes
+
+        char ch = 'a'; // 2 bytes
+        boolean bool = false;
+
+        System.out
+                .println(num + " " + by + " " + sh + " " + l + " " + d + " " + f + " " + ch + " " + bool);
+
+    }
+
+}
