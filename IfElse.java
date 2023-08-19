98f4f279bb696de09a9b36c01a7980d4b0df7189
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-08-20 00:22:20 +0530

    if else condition

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        int x = 18;
+        if (x == 18)
+            System.out.println("Age is 18");
+        else if (x > 18)
+            System.out.println("Age is greater than 18");
+        else
+            System.out.println("Age is less than 18");
+    }
+
+}
