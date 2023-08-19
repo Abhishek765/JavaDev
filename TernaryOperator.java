9dc11c061fb06952761a6314b2390fb6f778a0ce
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-08-20 00:26:53 +0530

    ternary operator

---
+++ TernaryOperator.java
@@ -0,0 +1,11 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int n = 11, result = 0;
+
+        result = n % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
+    }
+
+}
