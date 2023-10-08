0ebba1ac7da68bbf84e9542e5ce614efde71115f
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-10-08 20:39:35 +0530

    feat: multilevel inheritance

--- inheritance/DemoInheritance.java
+++ inheritance/DemoInheritance.java
@@ -3,12 +3,14 @@ package inheritance;
 public class DemoInheritance {
     public static void main(String[] args) {
 
-        AdvancedCalc advancedCalcObj = new AdvancedCalc();
-        int sum = advancedCalcObj.add(2, 3);
-        int sub = advancedCalcObj.sub(5, 3);
-        int mul = advancedCalcObj.multiply(5, 3);
-        int div = advancedCalcObj.divide(15, 3);
-        System.out.println(sum + " " + sub + " " + mul + " " + div);
+        ScientificCalc scientificCalcObj = new ScientificCalc();
+        int sum = scientificCalcObj.add(2, 3);
+        int sub = scientificCalcObj.sub(5, 3);
+        int mul = scientificCalcObj.multiply(5, 3);
+        int div = scientificCalcObj.divide(15, 3);
+        double powerOfTwo = scientificCalcObj.power(2, 3);
+        double piValue = scientificCalcObj.PI();
+        System.out.println(sum + " " + sub + " " + mul + " " + div + " " + powerOfTwo + " " + piValue);
     }
 
 }
---
+++ inheritance/ScientificCalc.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class ScientificCalc extends AdvancedCalc {
+
+    public double power(int n1, int n2) {
+        return Math.pow(n1, n2);
+    }
+
+    public double PI() {
+        return Math.PI;
+    }
+}
