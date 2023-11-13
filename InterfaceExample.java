374d4494b3019cac6ffbb3800c929a5f912462ae
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-11-13 22:56:37 +0530

    more on interfaces

--- InterfaceExample.java
+++ InterfaceExample.java
@@ -2,7 +2,9 @@
  * * In Interface by default all methods are `public abstract`
  * * We can't create objects of interface
  * * all variables are by default final and static
- * 
+ * * class - class = extends
+ * * class - interface = implements
+ * * interface - interface = extends
  */
 
 interface A {
@@ -15,7 +17,14 @@ interface A {
     void config();
 }
 
-class B implements A {
+interface X {
+    void run(); // ! by default methods in interface are public abstract
+}
+
+interface Y extends X {
+}
+
+class B implements A, Y {
 
     public void show() {
         System.out.println("In show B");
@@ -23,6 +32,10 @@ class B implements A {
 
     public void config() {
         System.out.println("In config B");
+    }
+
+    public void run() {
+        System.out.println("In run B");
     }
 
 }
@@ -35,6 +48,9 @@ public class InterfaceExample {
 
         obj.show();
         obj.config();
+
+        X obj1 = new B();
+        obj1.run();
 
         // A.age = 24 //! this is not possible because age is final
         System.out.println("A.age = " + A.age);
